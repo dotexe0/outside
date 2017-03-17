2f799c1ec783658001361de207d734e7d4870a22
Daniel Varela <dotexe0@gmail.com>
2017-03-16 22:09:38 -0700

    updated server with middlewares, morgan, bodyparser for json res

--- src/js/server/index.js
+++ src/js/server/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dbConfig from './config/db';
+import middlewareConfig from './config/middlewares';
+
 import eventsRouter from './modules/events/routes';
 import usersRouter from './modules/users/routes'
 
@@ -8,11 +10,13 @@ const app = express();
 // database
 dbConfig();
 
+//middlewares
+middlewareConfig(app);
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, err => {
   if(err) {
     console.error(err);
   }
-  console.log(`Server running on port ${3001}`);
+  console.log(`Server running on port ${PORT}`);
 });
