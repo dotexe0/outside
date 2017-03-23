12cab2e9f62357ea301cff2b27e71a252d9f5247
Daniel Varela <dotexe0@gmail.com>
2017-03-22 19:53:15 -0700

    working delete event in db controller/routes

--- backend/server/modules/events/controller.js
+++ backend/server/modules/events/controller.js
@@ -25,4 +25,14 @@ export const getAllEvents = async (req, res) => {
     console.log('Get all events error', error);
     return res.status(404).json({ error: true });
   }
-  };
+};
+
+export const deleteEvent = async (req, res) => {
+  try {
+    await Event.findOneAndRemove(req.params.id);
+    return res.sendStatus(200);
+  } catch (error) {
+    console.log('Get all events error', error);
+    return res.status(400).json({ error: true });
+  }
+};
