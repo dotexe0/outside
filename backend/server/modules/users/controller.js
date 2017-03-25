b991732454864323a295ed095af036e53f49472e
Daniel Varela <dotexe0@gmail.com>
2017-03-24 23:32:55 -0700

    backend controller for signup working

--- backend/server/modules/users/controller.js
+++ backend/server/modules/users/controller.js
@@ -11,12 +11,12 @@ export const userSignup = async (req, res) => {
     console.log('args: ', ...args);
     console.log('params', req.params);
     console.log('body', req.body);
-    const newUser = new User({ email: req.body.email, password: req.body.password });
+    const newUser = new User({ local: req.body });
     console.log('newUser: ', newUser);
     try {
-    res.sendStatus(200).json({ user: await newUser.save() });
+    res.status(200).json({ user: await newUser.save() });
   } catch (error) {
     console.log('User signup error: ', error);
-    return res.sendStatus(500);
+    return res.status(500);
   }
 };
