313244cbd0780086a02d9ef373bac780ee86a3c7
Daniel Varela <dotexe0@gmail.com>
2017-04-02 15:56:54 -0700

    updated server for build

--- backend/server/index.js
+++ backend/server/index.js
@@ -11,15 +11,43 @@ const app = express();
 //middlewares
 middlewareConfig(app);
 
-const staticFiles = express.static(path.join(__dirname, '../../frontend/build'));
-app.use(staticFiles);
-
 //end points
 app.use('/api', [EventRoutes, UserRoutes]);
-app.use('/*', staticFiles);
+app.use(express.static('../build'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 
 
 app.set('port', (process.env.PORT || 3001));
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`);
 });
+
+
+// import express from 'express';
+// import path from 'path';
+// import './config/db'; // database
+// import middlewareConfig from './config/middlewares';
+
+// import { EventRoutes } from './modules/events';
+// import { UserRoutes } from './modules/users';
+
+// const app = express();
+
+// //middlewares
+// middlewareConfig(app);
+
+// const staticFiles = express.static(path.join(__dirname, '../../frontend/build'));
+// app.use(staticFiles);
+
+// //end points
+// app.use('/api', [EventRoutes, UserRoutes]);
+// app.use('/*', staticFiles);
+
+
+// app.set('port', (process.env.PORT || 3001));
+// app.listen(app.get('port'), () => {
+//   console.log(`Listening on ${app.get('port')}`);
+// });
